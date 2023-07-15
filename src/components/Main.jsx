e06550f31aa7df07937acbe6e072787545317470
DarthRL <darthrlu@gmail.com>
2023-07-15 18:21:23 +0800

    10.5

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,23 @@
-import { Text, StyleSheet, View } from 'react-native';
-import RepositoryList from './RepositoryList';
-import AppBar from './AppBar';
+import { StyleSheet, View } from 'react-native'
+import RepositoryList from './RepositoryList'
+import AppBar from './AppBar'
+import theme from '../theme'
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: theme.colors.mainBackground,
   },
-});
+})
 
 const Main = () => {
   return (
     <View style={styles.container}>
-      <AppBar/>
-      <Text>Rate Repository Application</Text>
-      <RepositoryList/>
+      <AppBar />
+      <RepositoryList />
     </View>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,15 +1,109 @@
-import { Text, View } from 'react-native'
+import { Image, StyleSheet, View } from 'react-native'
+import Text from './Text'
+import theme from '../theme'
+
+const styles = StyleSheet.create({
+  flexColumnItemContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  flexRowHeadContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+
+  flexColumnDetailsContainer: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    marginTop: 10,
+    marginRight: 10,
+  },
+  flexRowAttributesContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    margin: 10,
+    flexWrap: 'wrap',
+  },
+  flexColumnAttributesContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  text: { padding: 2, flexShrink: 1 },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 3,
+    margin: 12,
+    marginBottom: 0,
+  },
+})
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View key={item.id}>
-      <Text>Full name: {item.fullName}</Text>
-      <Text>Description: {item.description}</Text>
-      <Text>Language: {item.language}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Forks: {item.forksCount}</Text>
-      <Text>Reviews: {item.reviewCount}</Text>
-      <Text>Rating: {item.ratingAverage}</Text>
+    <View
+      key={item.id}
+      style={[styles.flexColumnItemContainer, { backgroundColor: 'white' }]}
+    >
+      <View id='first-row' style={styles.flexRowHeadContainer}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: `https://github.com/${item.fullName.split('/')[0]}.png`,
+          }}
+        />
+        <View style={styles.flexColumnDetailsContainer}>
+          <Text style={styles.text} fontWeight={'bold'}>
+            {item.fullName}
+          </Text>
+          <Text style={styles.text}>{item.description}</Text>
+          <View style={{ paddingLeft: 2, paddingTop: 5 }}>
+            <Text
+              style={[
+                {
+                  color: 'white',
+                  backgroundColor: theme.colors.primary,
+                  borderRadius: 3,
+                  padding: 3,
+                },
+              ]}
+            >
+              {item.language}
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View id='second-row' style={styles.flexRowAttributesContainer}>
+        <View style={styles.flexColumnAttributesContainer}>
+          <Text style={styles.text} fontWeight={'bold'}>
+            {item.stargazersCount}
+          </Text>
+          <Text style={styles.text}>Stars</Text>
+        </View>
+        <View style={styles.flexColumnAttributesContainer}>
+          <Text style={styles.text} fontWeight={'bold'}>
+            {item.forksCount}
+          </Text>
+          <Text style={styles.text}>Forks</Text>
+        </View>
+        <View style={styles.flexColumnAttributesContainer}>
+          <Text style={styles.text} fontWeight={'bold'}>
+            {item.reviewCount}
+          </Text>
+          <Text style={styles.text}>Reviews</Text>
+        </View>
+        <View style={styles.flexColumnAttributesContainer}>
+          <Text style={styles.text} fontWeight={'bold'}>
+            {item.ratingAverage}
+          </Text>
+          <Text style={styles.text}>Rating</Text>
+        </View>
+      </View>
     </View>
   )
 }
