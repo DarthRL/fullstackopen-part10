be13fabe6ba4de69deee9ef90f967c0861f326e2
DarthRL <darthrlu@gmail.com>
2023-07-16 16:42:52 +0800

    10.15

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,6 +5,7 @@ import FormikTextInput from './FormikTesxtInput'
 import theme from '../theme'
 import * as yup from 'yup'
 import useSignIn from '../hooks/useSignIn'
+import { useNavigate } from 'react-router-native'
 
 const initialValues = {
   username: '',
@@ -72,13 +73,14 @@ const SignInForm = ({ onSubmit }) => {
 
 const SignIn = () => {
   const [signIn] = useSignIn()
+  const navigate = useNavigate()
 
   const onSubmit = async values => {
-    const {username, password} = values
+    const { username, password } = values
 
     try {
-      const {data} = await signIn({username, password})
-      console.log(data)
+      await signIn({ username, password })
+      navigate('/')
     } catch (e) {
       console.log(e)
     }
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,12 +1,16 @@
-import { useMutation } from '@apollo/client'
+import { useApolloClient, useMutation } from '@apollo/client'
 import { AUTHENTICATE } from '../graphql/queries'
+import { useAuthStorage } from '../contexts/AuthStorageContext'
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(AUTHENTICATE)
+  const authStorage = useAuthStorage()
+  const apolloClient = useApolloClient()
 
   const signIn = async ({ username, password }) => {
-    const result = await mutate({ variables: { username, password } })
-    return result
+    const { data } = await mutate({ variables: { username, password } })
+    await authStorage.setAccessToken(data.authenticate.accessToken)
+    apolloClient.resetStore()
   }
 
   return [signIn, result]
